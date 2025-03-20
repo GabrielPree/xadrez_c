4d23ee2cc3d27b00c800827a45b6b14e34be6e7c
Gabriel <gabriellopesp@hotmail.com>
2025-03-20 14:50:59 +0000

    Adicionado a movimentação do bispo

--- xadrez.c
+++ xadrez.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
  
 int main() {
-    int torre = 0;
+    //variaveis para armazenar a posicao das peças
+    int torre = 0, bispo = 0;
    
+    //movimentação da torre
     printf("Torre move-se para:\n");
     while (torre < 5) {
         printf("Direita\n");
        torre++;
     }
    
+    //movimentação do bispo
+    printf("\nBispo move-se para:\n");
+    do{
+        printf("Cima, Direita\n");
+        bispo++;
+    } while (bispo < 5);
+
    
     return 0;
 }
