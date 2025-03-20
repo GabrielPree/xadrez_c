a16bbbf097e842e374d5e47a0db75384ea160b8a
Gabriel <gabriellopesp@hotmail.com>
2025-03-20 14:47:13 +0000

    Começo do jogo de xadrez ja adicionado o movimento da torre.

---
+++ xadrez.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main() {
+    int torre = 0;
+   
+    printf("Torre move-se para:\n");
+    while (torre < 5) {
+        printf("Direita\n");
+       torre++;
+    }
+   
+   
+    return 0;
+}
