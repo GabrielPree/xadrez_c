e2f7dff0cbc2a5d1b4987be6d847c87f2b910eb9
Gabriel <gabriellopesp@hotmail.com>
2025-03-21 15:13:06 +0000

    Adicionado a movimentação do cavalo

--- xadrez.c
+++ xadrez.c
@@ -2,7 +2,7 @@
  
 int main() {
     //variaveis para armazenar a posicao das peças
-    int torre = 0, bispo = 0, rainha = 0;
+    int torre = 0, bispo = 0, rainha = 0, cavalo = 1;
    
     //movimentação da torre
     printf("Torre move-se para:\n");
@@ -21,6 +21,15 @@ int main() {
     //movimentação da rainha
     printf("\nRainha move-se para:\n");
     for (rainha; rainha < 8; rainha++) {
+        printf("Esquerda\n");
+    }
+
+    //movimentação do cavalo
+    printf("\nCavalo move-se para:\n");
+    while(cavalo--) {
+        for(int i = 0; i < 2; i++) {
+            printf("Baixo\n");
+        }
         printf("Esquerda\n");
     }
     
